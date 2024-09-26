6de85791a76e700b9d68a73d2482d3ed13461180
saepudinasep <saepudinasep2001@gmail.com>
2024-09-26 08:35:52 +0700

    [ADD] Menambahkan migrate kas & branch

---
+++ database/migrations/2024_09_26_013415_create_branches_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('branches', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('id_kas');
+            $table->foreign('id_kas')->references('id')->on('kas');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('branches');
+    }
+};
